cd9336e53c5873ba03a7c62eef6f940fbf5f25c9
Alexander <sapfirmoscow@yandex.ru>
2018-12-29 06:20:40 +0300

    Initial commit

---
+++ app/src/main/java/ru/sberbank/homework6/MainActivity.java
@@ -0,0 +1,24 @@
+package ru.sberbank.homework6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycle_view);
+        layoutManager = new LinearLayoutManager(this);
+
+
+    }
+}
---
+++ app/src/main/java/ru/sberbank/homework6/MyIntentService.java
@@ -0,0 +1,16 @@
+package ru.sberbank.homework6;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //code here
+    }
+}
